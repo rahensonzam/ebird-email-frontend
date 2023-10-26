bb1e770aa420bd23bbdd4be637c6b64c27a26987
rahensonzam <rahensonzam@gmail.com>
2023-10-26 22:11:49 +0200

    http request

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ let map;
 
 document.getElementById("myInput").addEventListener("keyup", filterFunction)
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+async function makeWebRequest(url) {
+    const data = await fetch(url);
+    const res = await data.json();
+    return res;
+}
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    const birdData = await makeWebRequest("http://127.0.0.1:3000/api")
+
+    console.log(birdData)
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
 }
 
 initMap();
