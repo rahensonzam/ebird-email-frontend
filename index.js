9c300c52c450778dcc412e1fb9385d7ea84dbea1
rahensonzam <rahensonzam@gmail.com>
2023-11-02 20:54:57 +0200

    group markers on same point

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function initMap() {
 
     const birdData = await makeWebRequest("http://127.0.0.1:3000/api");
 
-    console.log(birdData);
+    // console.log(birdData);
 
     const centerPosition = { lat: -17.8452628, lng: 25.803017 };
 
@@ -24,21 +24,58 @@ async function initMap() {
         mapId: "DEMO_MAP_ID"
     });
 
+    let markersNested = [];
     let markers = [];
 
     // Create an info window to share between markers.
     const infoWindow = new InfoWindow();
 
-    birdData.forEach((bird) => {
+    outerLoop: for (let indexA = 0; indexA <= birdData.length - 1; indexA++) {
+        const bird = birdData[indexA];
+        if (indexA === 0) {
+            markersNested.push([birdData[indexA]]);
+            continue;
+        }
+        for (let indexB = 0; indexB <= markersNested.length - 1; indexB++) {
+            const nestedMarker = markersNested[indexB][0];
+            if (bird.lat1 === nestedMarker.lat1 && bird.lng1 === nestedMarker.lng1) {
+                markersNested[indexB].push(birdData[indexA]);
+                continue outerLoop;
+            }
+        }
+        markersNested.push([birdData[indexA]]);
+    }
+
+    console.log("markersNested", markersNested);
+
+    // birdData.forEach((bird) => {
+    for (let indexA = 0; indexA <= markersNested.length - 1; indexA++) {
+        const birdPos = markersNested[indexA][0];
+
+        let birdTitle = "";
+        if (markersNested[indexA].length > 1) {
+            birdTitle = "Various";
+        } else {
+            birdTitle = markersNested[indexA][0].commonname;
+        }
+
+        let contentString = `<div id="popupContent">`;
+        for (let indexB = 0; indexB <= markersNested[indexA].length - 1; indexB++) {
+            const bird = markersNested[indexA][indexB];
+            contentString += `<p>${bird.commonname} <i>${bird.scientificname}</i><br>
+            <b>Date Reported:</b> ${bird.datereported} <b>Reported By:</b> ${bird.reportedby}<br>
+            <b>Location:</b> ${bird.locationname}<br>
+            <b>Checklist:</b> <a href="${bird.checklistlink}">${bird.checklistlink}</a></p>`;
+        }
+        contentString += `</div>`;
+
+        // console.log("contentString", contentString);
+
         const marker = new AdvancedMarkerElement({
             map: map,
-            position: { lat: bird.lat1, lng: bird.lng1 },
-            title: bird.commonname,
+            position: { lat: birdPos.lat1, lng: birdPos.lng1 },
+            title: birdTitle,
         });
-
-        markers.push(marker);
-
-        let contentString = `<p>Common Name%3A ${bird.commonname}\nScientific Name%3A ${bird.scientificname}\nDate Reported%3A ${bird.datereported}\nReported By%3A ${bird.reportedby}\nLocation%3A ${bird.locationname}\nLat%3A ${bird.lat1}\nLng%3A ${bird.lng1}\nChecklist%3A <a href="${bird.checklistlink}">${bird.checklistlink}</a></p>`;
 
         marker.addListener("click", () => {
             infoWindow.close();
@@ -47,7 +84,14 @@ async function initMap() {
             infoWindow.open({ anchor: marker, map });
         });
 
-    });
+        markers.push(marker);
+
+    }
+    // });
+
+    console.log("birdData", birdData);
+    console.log("markers", markers);
+
 }
 
 initMap();
